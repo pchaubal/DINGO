77ca7a9bf4ce195851c23b1258cfe3128e1206e4
pchaubal <prakrutchaubal@gmail.com>
2019-09-28 14:56:04 +1000

    MH seems to be working

--- mcmc.py
+++ mcmc.py
@@ -3,83 +3,47 @@ from numba import jit
 from samplers import sampler_class
 
 def model(model_params):
-	a=model_params[0]
-	b=model_params[1]
-	return (a + b**2)
+	a=model_params[0] + 0.01*np.random.standard_normal()
+	b=model_params[1] + 0.01*np.random.standard_normal()
+	return (2*a + b**2)
 
 
 def loglikelihood(model_params):
 	chi_sq = np.sum((data-model(model_params))**2)
 	sigma = np.std(data)
-	C = sigma**2 * data.shape[0]
 	
 	# The loglikelihood
-	lnL = - 0.5*chi_sq/C
+	lnL = - 0.5*chi_sq/(len(data)*sigma**2)
+	# print (lnL)
 	return lnL
-
-# @jit()
-# def sampler(n_samp_max, data):
-# 	new_lnlik = 0.0001 
-# 	old_lnlik = 0.0
-# 	accepted_points = []
-# 	alpha = 5
-# 	beta = 3
-# 	while (len(accepted_points) < n_samp_max):
-# 		# pick a random point to start in your model space
-# 		delta = 0.01
-# 		alpha = alpha + delta*np.random.standard_normal()
-# 		beta = beta + delta*np.random.standard_normal()
-
-# 		model_params = [alpha,beta]
-# 		new_lnlik = loglikelihood(data,model_params)
-# 		# print new_lnlik
-
-# 		if (new_lnlik > old_lnlik) :# Accept this point 
-# 			accepted_points.append(model_params)
-# 			print '\naccepted'
-# 			print len(accepted_points), model_params
-# 			old_lnlik=new_lnlik
-# 		else: # accept with probability L_new/L_old
-# 			# print new_lnlik, old_lnlik,
-# 			p = np.exp((new_lnlik-old_lnlik)/old_lnlik)
-# 			# print '\nalpha = ', alpha, 'beta =', beta
-# 			# print 'may accept with p = ', p
-# 			if (np.random.random_sample() < p):
-# 				accepted_points.append(model_params)
-# 				# print 'accept'
-# 				print len(accepted_points), model_params
-# 				old_lnlik=new_lnlik
-# 			else:
-# 				# print 'rejected'
-# 				pass
-
-# 	return np.array(accepted_points)
 
 npoints = 5000
 # x = np.random.randn(npoints) # 1000 linearly spaced points in [0,10]
 data_params = [5,3]
-noise = 0.1*np.random.standard_normal(npoints)
-data = np.repeat(model(data_params), npoints) + noise
+noise = 0.10*np.random.standard_normal(npoints)
+data = [model(data_params) for i in range(npoints)] + noise
 
-samples = sampler_class.MetropolisHastings(npoints,loglikelihood)
-
-# alpha = np.linspace(0,10,1000)
-# lnL = np.empty(1000)
-# for index,alp in enumerate(alpha):
-# 	model_params=[alp,3]
-# 	lnL[index] = loglikelihood(data, model_params)
-
-# lnL -= lnL.max()
+# print(data)
 
 
 # # The sampling algorithm parameters
-n_samp_max = 100
-sampled_points = sampler(n_samp_max,data)
+n_samp_max = 1000
+samples = sampler_class.MetropolisHastings(n_samp_max,loglikelihood)
 
-alpha = sampled_points[:,0]
-beta = sampled_points[:,1]
-
+alpha = samples[:,0]
+beta = samples[:,1]
 import matplotlib.pyplot as plt
-plt.hist(alpha, bins=20)
+plt.scatter(alpha,beta, s=0.5)
+plt.axhline(3.0,color='k', label='true value')
+plt.axvline(5.0,color='k')
+
+plt.axvline(alpha.mean(),color='k', linestyle='--')
+plt.axhline(beta.mean(),color='k', linestyle='--')
+# plt.plot(data)
+# plt.plot(alpha/5.0)
+# plt.plot(beta/3.0)
+# plt.hist(alpha,bins=20)
+# plt.hist(beta,bins=20)
+
 # plt.plot(alpha,np.exp(lnL))
 plt.show()
--- samplers.py
+++ samplers.py
@@ -6,36 +6,45 @@ class sampler_class(object):
 		pass
 
 	def MetropolisHastings(n_samples, loglikelihood):
-		new_lnlik = 0.0001  #Initialize the value to some small value
-		old_lnlik = 0.0 	#Initializing
+		initial_guess = [4,2]
+		old_lnlik = loglikelihood(initial_guess) 	#Initializing
 		accepted_points = []
-		alpha = 5	#First guess
-		beta = 3	#First guess
+		
 		while (len(accepted_points) < n_samples):
-			# pick a random point to start in your model space
-			delta = 0.01 # the size of step
-			alpha = alpha + delta*np.random.standard_normal()
-			beta = beta + delta*np.random.standard_normal()
+			old_point = np.array(initial_guess)
+			delta = min(1.0,-old_lnlik) # the size of step
+			new_point = old_point + delta*np.random.standard_normal(old_point.shape)
+			# alpha = alpha + delta*np.random.standard_normal()
+			# beta = beta + delta*np.random.standard_normal()
 
-			model_params = [alpha,beta]
-			new_lnlik = loglikelihood(model_params)
+			new_lnlik = loglikelihood(new_point)
 			# print new_lnlik
 
 			if (new_lnlik > old_lnlik) :# Accept this point 
-				accepted_points.append(model_params)
-				print '\naccepted'
-				print len(accepted_points), model_params
+				accepted_points.append(new_point)
+				# print ('\naccepted')
+				print('\nL=',new_lnlik)
+				print (len(accepted_points), new_point)
+				print(delta)
+
 				old_lnlik=new_lnlik
+				old_point = new_point
 			else: # accept with probability L_new/L_old
-				# print new_lnlik, old_lnlik,
-				p = np.exp((new_lnlik-old_lnlik)/old_lnlik)
-				# print '\nalpha = ', alpha, 'beta =', beta
-				# print 'may accept with p = ', p
+				p = np.exp(new_lnlik-old_lnlik)
+				# p=0.0
+
+				# if (p!=0):
+				# 	print ('newL, oldL:',new_lnlik, old_lnlik,)
+				# 	print ('may accept with p = ', p)
+
 				if (np.random.random_sample() < p):
-					accepted_points.append(model_params)
-					# print 'accept'
-					print len(accepted_points), model_params
+					accepted_points.append(new_point)
+					# print ('accept')
+					print('\nL=',new_lnlik)
+					print (len(accepted_points), new_point)
+					print(delta)
 					old_lnlik=new_lnlik
+					old_point=new_point
 				else:
 					# print 'rejected'
 					pass
