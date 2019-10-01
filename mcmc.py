6cfbc2a8abdd9380d89f602eea9552855192515f
Prakrut Chaubal <prakrutchaubal@gmail.com>
2019-10-01 17:38:53 +1000

    Added a reference dir and minor updates

--- mcmc.py
+++ mcmc.py
@@ -1,49 +1,53 @@
 import numpy as np
-from numba import jit
 from samplers import sampler_class
 
 def model(model_params):
-	a=model_params[0] + 0.01*np.random.standard_normal()
-	b=model_params[1] + 0.01*np.random.standard_normal()
-	return (2*a + b**2)
+	a=model_params[0]
+	b=model_params[1]
+	return (a + a*b**2 + a**3)
 
 
 def loglikelihood(model_params):
-	chi_sq = np.sum((data-model(model_params))**2)
 	sigma = np.std(data)
+	chi_sq = np.sum((data-model(model_params))**2/sigma**2)
+	# C = sigma**2
 	
 	# The loglikelihood
-	lnL = - 0.5*chi_sq/(len(data)*sigma**2)
-	# print (lnL)
+	lnL = - 0.5*chi_sq/5000.0
 	return lnL
 
 npoints = 5000
 # x = np.random.randn(npoints) # 1000 linearly spaced points in [0,10]
-data_params = [5,3]
-noise = 0.10*np.random.standard_normal(npoints)
-data = [model(data_params) for i in range(npoints)] + noise
+data_params = np.asarray([5,3])
+noise = 0.1*np.random.standard_normal(npoints)
+data = [model(data_params + 0.1*np.random.standard_normal(data_params.shape)) for i in range(npoints)] + noise
 
-# print(data)
-
-
-# # The sampling algorithm parameters
+#sampler parameters
 n_samp_max = 1000
-samples = sampler_class.MetropolisHastings(n_samp_max,loglikelihood)
+initial_guess = [3,2]
+priors = [1.0,1.0]
 
-alpha = samples[:,0]
-beta = samples[:,1]
+sampler = sampler_class()
+sampled_points = sampler.MetropolisHastings(n_samp_max, initial_guess, priors,loglikelihood)
+
+
+
+# ################################
+#  Plotting module. Should be generalized and shifted at some point
+
+
+alpha = sampled_points[:,0]
+beta = sampled_points[:,1]
+
 import matplotlib.pyplot as plt
-plt.scatter(alpha,beta, s=0.5)
-plt.axhline(3.0,color='k', label='true value')
-plt.axvline(5.0,color='k')
+plt.subplot(224)
+plt.hist(alpha, density=True, bins=20)
 
-plt.axvline(alpha.mean(),color='k', linestyle='--')
-plt.axhline(beta.mean(),color='k', linestyle='--')
-# plt.plot(data)
-# plt.plot(alpha/5.0)
-# plt.plot(beta/3.0)
-# plt.hist(alpha,bins=20)
-# plt.hist(beta,bins=20)
+plt.subplot(221)
+plt.hist(beta, density=True, orientation='horizontal', bins=20)
 
-# plt.plot(alpha,np.exp(lnL))
+plt.subplot(222)
+plt.scatter(alpha,beta,s=0.5)
+plt.axhline(beta.mean(), color='k', linewidth=0.5)
+plt.axvline(alpha.mean(), color='k', linewidth=0.5)
 plt.show()
--- samplers.py
+++ samplers.py
@@ -2,51 +2,70 @@ import numpy as np
 
 class sampler_class(object):
 	"""docstring for sampler_class"""
-	def __init__(self, arg):
+	def __init__(self):
 		pass
 
-	def MetropolisHastings(n_samples, loglikelihood):
-		initial_guess = [4,2]
+	def MetropolisHastings(self,n_samples,initial_guess,priors, loglikelihood):
+		print_after_n = 100
+
 		old_lnlik = loglikelihood(initial_guess) 	#Initializing
 		accepted_points = []
 		
 		while (len(accepted_points) < n_samples):
 			old_point = np.array(initial_guess)
+
+			# This is the proposal density
 			delta = min(1.0,-old_lnlik) # the size of step
 			new_point = old_point + delta*np.random.standard_normal(old_point.shape)
-			# alpha = alpha + delta*np.random.standard_normal()
-			# beta = beta + delta*np.random.standard_normal()
+			new_point = old_point + np.random.normal(old_point, priors)
+			
+
 
 			new_lnlik = loglikelihood(new_point)
 			# print new_lnlik
 
-			if (new_lnlik > old_lnlik) :# Accept this point 
+			p = np.exp(new_lnlik-old_lnlik)
+			pi = min(1.0,p)
+
+			if (np.random.random_sample() < pi):
+				# Accept
 				accepted_points.append(new_point)
-				# print ('\naccepted')
-				print('\nL=',new_lnlik)
-				print (len(accepted_points), new_point)
-				print(delta)
-
-				old_lnlik=new_lnlik
+				old_lnlik = new_lnlik
 				old_point = new_point
-			else: # accept with probability L_new/L_old
-				p = np.exp(new_lnlik-old_lnlik)
-				# p=0.0
 
-				# if (p!=0):
-				# 	print ('newL, oldL:',new_lnlik, old_lnlik,)
-				# 	print ('may accept with p = ', p)
-
-				if (np.random.random_sample() < p):
-					accepted_points.append(new_point)
-					# print ('accept')
+				if (len(accepted_points)%print_after_n==0):
 					print('\nL=',new_lnlik)
 					print (len(accepted_points), new_point)
-					print(delta)
-					old_lnlik=new_lnlik
-					old_point=new_point
-				else:
-					# print 'rejected'
-					pass
+
+
+
+			# if (new_lnlik > old_lnlik) :# Accept this point 
+			# 	accepted_points.append(new_point)
+			# 	# print ('\naccepted')
+			# 	print('\nL=',new_lnlik)
+			# 	print (len(accepted_points), new_point)
+			# 	print(delta)
+
+			# 	old_lnlik=new_lnlik
+			# 	old_point = new_point
+			# else: # accept with probability L_new/L_old
+			# 	p = np.exp(new_lnlik-old_lnlik)
+			# 	# p=0.0
+
+			# 	# if (p!=0):
+			# 	# 	print ('newL, oldL:',new_lnlik, old_lnlik,)
+			# 	# 	print ('may accept with p = ', p)
+
+			# 	if (np.random.random_sample() < p):
+			# 		accepted_points.append(new_point)
+			# 		# print ('accept')
+			# 		print('\nL=',new_lnlik)
+			# 		print (len(accepted_points), new_point)
+			# 		print(delta)
+			# 		old_lnlik=new_lnlik
+			# 		old_point=new_point
+			# 	else:
+			# 		# print 'rejected'
+			# 		pass
 
 		return np.array(accepted_points)
